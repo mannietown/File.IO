53106d36dfd8665461fa6abe76a1e720d83a775e
MarkSFrancis <mrkfrancis8@gmail.com>
2016-05-15 12:24:34 +0100

    Making Conflict

--- Project 1/Project 1/Program.cs
+++ Project 1/Project 1/Program.cs
@@ -11,8 +11,8 @@ namespace Project_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("it's up i think.");
-            Console.ReadLine();
+            Console.WriteLine('\a');
+            Console.Read();
         }
     }
 }
