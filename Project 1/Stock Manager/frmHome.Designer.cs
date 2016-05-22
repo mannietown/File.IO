3bde29d56209ea569850262608d5b6ece57c7449
MarkSFrancis <mrkfrancis8@gmail.com>
2016-05-22 13:10:47 +0100

    Created DataGridView with DataEntry class. Save and Load have not been implemented

--- Project 1/Stock Manager/frmHome.Designer.cs
+++ Project 1/Stock Manager/frmHome.Designer.cs
@@ -29,40 +29,134 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.objectDescriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastStockAddedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.currentStockDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bsDatabase = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.numPrice = new System.Windows.Forms.NumericUpDown();
+            this.numStock = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bsDatabase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStock)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvMain
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.DataSource = this.bsDatabase;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 35);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(589, 368);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvMain.AutoGenerateColumns = false;
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.objectDescriptionDataGridViewTextBoxColumn,
+            this.lastStockAddedDataGridViewTextBoxColumn,
+            this.currentStockDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn});
+            this.dgvMain.DataSource = this.bsDatabase;
+            this.dgvMain.Location = new System.Drawing.Point(13, 12);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.Size = new System.Drawing.Size(589, 368);
+            this.dgvMain.TabIndex = 0;
+            // 
+            // objectDescriptionDataGridViewTextBoxColumn
+            // 
+            this.objectDescriptionDataGridViewTextBoxColumn.DataPropertyName = "ObjectDescription";
+            this.objectDescriptionDataGridViewTextBoxColumn.HeaderText = "ObjectDescription";
+            this.objectDescriptionDataGridViewTextBoxColumn.Name = "objectDescriptionDataGridViewTextBoxColumn";
+            // 
+            // lastStockAddedDataGridViewTextBoxColumn
+            // 
+            this.lastStockAddedDataGridViewTextBoxColumn.DataPropertyName = "LastStockAdded";
+            this.lastStockAddedDataGridViewTextBoxColumn.HeaderText = "LastStockAdded";
+            this.lastStockAddedDataGridViewTextBoxColumn.Name = "lastStockAddedDataGridViewTextBoxColumn";
+            this.lastStockAddedDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // currentStockDataGridViewTextBoxColumn
+            // 
+            this.currentStockDataGridViewTextBoxColumn.DataPropertyName = "CurrentStock";
+            this.currentStockDataGridViewTextBoxColumn.HeaderText = "CurrentStock";
+            this.currentStockDataGridViewTextBoxColumn.Name = "currentStockDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // bsDatabase
+            // 
+            this.bsDatabase.DataSource = typeof(Stock_Manager.DataEntry);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(527, 386);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 45);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.Location = new System.Drawing.Point(13, 391);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(255, 31);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // numPrice
+            // 
+            this.numPrice.DecimalPlaces = 2;
+            this.numPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numPrice.Location = new System.Drawing.Point(274, 392);
+            this.numPrice.Name = "numPrice";
+            this.numPrice.Size = new System.Drawing.Size(122, 31);
+            this.numPrice.TabIndex = 3;
+            // 
+            // numStock
+            // 
+            this.numStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numStock.Location = new System.Drawing.Point(402, 391);
+            this.numStock.Name = "numStock";
+            this.numStock.Size = new System.Drawing.Size(119, 31);
+            this.numStock.TabIndex = 4;
             // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(614, 415);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(614, 443);
+            this.Controls.Add(this.numStock);
+            this.Controls.Add(this.numPrice);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvMain);
             this.Name = "frmHome";
             this.Text = "Home";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bsDatabase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStock)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvMain;
         private System.Windows.Forms.BindingSource bsDatabase;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.NumericUpDown numPrice;
+        private System.Windows.Forms.NumericUpDown numStock;
+        private System.Windows.Forms.DataGridViewTextBoxColumn objectDescriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastStockAddedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn currentStockDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
     }
 }
 
--- Project 1/Stock Manager/frmHome.cs
+++ Project 1/Stock Manager/frmHome.cs
@@ -1,40 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
 
 namespace Stock_Manager
 {
     public partial class frmHome : Form
     {
+        List<DataEntry> MainDatabase;
         public frmHome()
         {
             InitializeComponent();
+            try
+            {
+                MainDatabase = DataIO.Load();
+            }
+            catch (FileNotFoundException)
+            { MainDatabase = new List<DataEntry>(); }
 
+            bsDatabase.DataSource = MainDatabase;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            MainDatabase.Add(new DataEntry(txtDescription.Text, (double)numPrice.Value, (int)numStock.Value));
+            
+            bsDatabase.DataSource = null;
+            bsDatabase.DataSource = MainDatabase;
         }
     }
 
-    class DataClass
+    public class DataEntry
     {
         string objectdescription;
-        public string ObjectDescription { get { return objectdescription; } set { } }
+        public string ObjectDescription { get { return objectdescription; } set { objectdescription = value; } }
+        DateTime laststockadded;
+        public DateTime LastStockAdded { get { return laststockadded; } }
+        private int currentstock = 0;
+        public int CurrentStock
+        {
+            get { return currentstock; }
+            set
+            {
+                if (value > currentstock)
+                {
+                    laststockadded = DateTime.Now;
+                    currentstock = value;
+                }
+                else
+                    currentstock = value;
+            }
+        }
+
+        private double price = 0;
+        public double Price { get { return price; } set { price = Math.Round(value, 2); } }
+
+        public DataEntry()
+        {
+            objectdescription = "";
+            currentstock = 0;
+        }
+
+        public DataEntry(string ObjectDescription, double Price, int CurrentStock)
+        {
+            objectdescription = ObjectDescription;
+            this.Price = Price;
+            laststockadded = DateTime.Now;
+            this.CurrentStock = CurrentStock;
+        }
+
+        public double ApplyDiscount(int PercentageOff)
+        {
+            Price = Price * ((100 - PercentageOff) / 100);
+            return Price;
+        }
     }
 
     static class DataIO
     {
-        public static void Load()
-        {
+        private static string FileLocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stock Manager\\Database.dat";
 
+        public static List<DataEntry> Load()
+        {
+            if (!File.Exists(FileLocation))
+                throw new FileNotFoundException("Database file not found: " + Environment.NewLine + FileLocation);
+
+            using (FileStream reader = new FileStream(FileLocation, FileMode.Open, FileAccess.Read))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.TypeFormat = System.Runtime.Serialization.Formatters.FormatterTypeStyle.TypesWhenNeeded;
+
+                return (List<DataEntry>)bf.Deserialize(reader);
+            }
         }
 
-        public static void Save()
+        public static void Save(List<DataEntry> EntriesToSave)
         {
+            string Folder = FileLocation.Substring(0, FileLocation.LastIndexOf('\\'));
+            if (!Directory.Exists(Folder))
+                Directory.CreateDirectory(Folder);
 
+            using (FileStream writer = new FileStream(FileLocation, FileMode.Create, FileAccess.Write))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.TypeFormat = System.Runtime.Serialization.Formatters.FormatterTypeStyle.TypesWhenNeeded;
+
+                bf.Serialize(writer, EntriesToSave);
+            }
         }
     }
 }
